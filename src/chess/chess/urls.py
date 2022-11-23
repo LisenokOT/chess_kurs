ccc7020b54057dde38bdd6c4e07c90d0e06cb8d7
LisenokOT <t4rarina-o@yandex.ru>
2022-11-23 19:06:50 +0300

    start page

--- src/chess/chess/urls.py
+++ src/chess/chess/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from game.views import index_p
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_p),
 ]
