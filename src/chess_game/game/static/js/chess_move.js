d9c83bfa68351aecfac919b4cb836b8c49084185
LisenokOT <t4rarina-o@yandex.ru>
2022-11-29 13:18:51 +0300

    board and figure

--- src/chess_game/game/static/js/chess_move.js
+++ src/chess_game/game/static/js/chess_move.js
@@ -9,7 +9,6 @@ list_stroke[count_stroke] = Object.assign({}, map);
 
 
 $(function () {
-    table_create();
     setDraggable();
     setDroppable();
 });
@@ -49,78 +48,78 @@ function moveFigure(fromPoly, toPoly){
 }
 
 
-function table_create(){
-    console.log('table');
-    let table = document.createElement("table");
-    table.className = "table_chess";
-    let container = document.getElementsByClassName("container_chess")[0];
-    for (let i = 0; i < 8; i++) {
-        let tr = document.createElement('tr');
-        for (let j = 0; j < 8; j++) {
-            let td = document.createElement('td');
-            if (i%2 == j%2) {
-                td.className = "square white";
-                td.id = String(i * 8 + j);
-                td.innerHTML = "";
-            }
-            else {
-                td.className = "square black";
-                td.id = String(i * 8 + j);
-                td.innerHTML = "";
-            }
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    }
-    container.appendChild(table);
-    figure_create();
-}
+// function table_create(){
+//     console.log('table');
+//     let table = document.createElement("table");
+//     table.className = "table_chess";
+//     let container = document.getElementsByClassName("container_chess")[0];
+//     for (let i = 0; i < 8; i++) {
+//         let tr = document.createElement('tr');
+//         for (let j = 0; j < 8; j++) {
+//             let td = document.createElement('td');
+//             if (i%2 == j%2) {
+//                 td.className = "square white";
+//                 td.id = String(i * 8 + j);
+//                 td.innerHTML = "";
+//             }
+//             else {
+//                 td.className = "square black";
+//                 td.id = String(i * 8 + j);
+//                 td.innerHTML = "";
+//             }
+//             tr.appendChild(td);
+//         }
+//         table.appendChild(tr);
+//     }
+//     container.appendChild(table);
+//     figure_create();
+// }
 
-function figure_create() {
-    console.log('figure');
-    for (let i = 0; i < 64; i++) {
-        let tds = document.getElementById(i);
-        tds.innerHTML = "";
-        tds.innerHTML = getFigures(map[i], i);
-    }
-    setDraggable();
-}
+// function figure_create() {
+//     console.log('figure');
+//     for (let i = 0; i < 64; i++) {
+//         let tds = document.getElementById(i);
+//         tds.innerHTML = "";
+//         tds.innerHTML = getFigures(map[i], i);
+//     }
+//     setDraggable();
+// }
 
-function getFigures(figure, cord){
-    if(localStorage.getItem('figure') == "images"){
-        switch (figure){
-            case 'k' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_king.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'q' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_queen.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'r' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_rock.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'n' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_knight.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'b' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_bishop.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'p' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_pawn.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+// function getFigures(figure, cord){
+//     if(localStorage.getItem('figure') == "images"){
+//         switch (figure){
+//             case 'k' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_king.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'q' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_queen.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'r' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_rock.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'n' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_knight.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'b' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_bishop.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'p' : return "<div class='figure-black' name='$cord'><img src='{% static 'images/black_pawn.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
 
-            case 'K' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_king.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'Q' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_queen.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'R' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_rock.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'N' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_knight.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'B' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_bishop.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case 'P' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_pawn.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
-            case '0' : return "";
-        }
-    }
-    else if (localStorage.getItem('figure') == "ascii"){
-        switch (figure){
-            case 'k' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9818;</div>".replace('$cord', cord);
-            case 'q' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9819;</div>".replace('$cord', cord);
-            case 'r' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9820;</div>".replace('$cord', cord);
-            case 'n' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9822;</div>".replace('$cord', cord);
-            case 'b' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9821;</div>".replace('$cord', cord);
-            case 'p' : return "<div class='figure-black' name='$cord' style='padding-bottom: 15px'>&#9823;</div>".replace('$cord', cord);
+//             case 'K' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_king.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'Q' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_queen.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'R' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_rock.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'N' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_knight.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'B' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_bishop.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case 'P' : return "<div class='figure-white' name='$cord'><img src='{% static 'images/white_pawn.png' %} width='60px' height='60px'></div>".replace('$cord', cord);
+//             case '0' : return "";
+//         }
+//     }
+//     else if (localStorage.getItem('figure') == "ascii"){
+//         switch (figure){
+//             case 'k' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9818;</div>".replace('$cord', cord);
+//             case 'q' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9819;</div>".replace('$cord', cord);
+//             case 'r' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9820;</div>".replace('$cord', cord);
+//             case 'n' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9822;</div>".replace('$cord', cord);
+//             case 'b' : return "<div class='figure-black' name='$cord' style='padding-bottom: 20px'>&#9821;</div>".replace('$cord', cord);
+//             case 'p' : return "<div class='figure-black' name='$cord' style='padding-bottom: 15px'>&#9823;</div>".replace('$cord', cord);
 
-            case 'K' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9812;</div>".replace('$cord', cord);
-            case 'Q' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9813;</div>".replace('$cord', cord);
-            case 'R' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9814;</div>".replace('$cord', cord);
-            case 'N' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9816;</div>".replace('$cord', cord);
-            case 'B' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9815;</div>".replace('$cord', cord);
-            case 'P' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9817;</div>".replace('$cord', cord);
-            case '0' : return "";
-        }
-    }
-}
+//             case 'K' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9812;</div>".replace('$cord', cord);
+//             case 'Q' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9813;</div>".replace('$cord', cord);
+//             case 'R' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9814;</div>".replace('$cord', cord);
+//             case 'N' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9816;</div>".replace('$cord', cord);
+//             case 'B' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9815;</div>".replace('$cord', cord);
+//             case 'P' : return "<div class='figure-white' name='$cord' style='padding-bottom: 20px'>&#9817;</div>".replace('$cord', cord);
+//             case '0' : return "";
+//         }
+//     }
+// }
--- src/chess_game/game/views.py
+++ src/chess_game/game/views.py
@@ -2,6 +2,33 @@ from django.shortcuts import render
 import chess
 import chess.svg
 
+def make_board():
+    board_ch = chess.Board()
+    array = board_ch.fen().split('/')
+    board = list()
+    for x in range(0,8):
+        tmp_objs = list()
+        for t in range(0,8):
+            cell = {}
+            if x%2 == 0:
+                if t%2 == 0:
+                    cell['color'] = "bg-black"
+                else:
+                    cell['color'] = "bg-white"
+            else:
+                if t%2 == 0:
+                    cell['color'] = "bg-white"
+                else:
+                    cell['color'] = "bg-black"
+            if len(array[x]) > t:
+                if array[x][t].isdigit():
+                    cell['fig'] = ""
+                else:
+                    cell['fig'] = array[x][t]
+            tmp_objs.append(cell)
+        board.append(list(tmp_objs))
+    return board
+
 def index_p(request):
     return render(request, 'index.html', {})
 
@@ -12,6 +39,6 @@ def index_log(request):
     return render(request, 'login.html', {})
 
 def index_chess(request):
-    board_ch = chess.Board()
     contex = {}
+    contex['board'] = make_board()
     return render(request, 'chess.html', contex)
